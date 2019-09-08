7847b25ba8245c804015bd8895773e46f7c5fc8e
yuiffy <yuiffy@qq.com>
2019-09-08 19:53:22 +0800

    Updates

---
+++ precache-manifest.52b6265f6aba9081285327fc20e8003a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1804bbba888621f3a396647184ef0b0b",
+    "url": "/bear-frog-chess/index.html"
+  },
+  {
+    "revision": "f328d9461ed0b97928c8",
+    "url": "/bear-frog-chess/static/css/main.8aa4b68d.chunk.css"
+  },
+  {
+    "revision": "30b0e662e91e2761b608",
+    "url": "/bear-frog-chess/static/js/2.58dc2f21.chunk.js"
+  },
+  {
+    "revision": "f328d9461ed0b97928c8",
+    "url": "/bear-frog-chess/static/js/main.5f7c4b41.chunk.js"
+  },
+  {
+    "revision": "7f030adf948aee43c3d7",
+    "url": "/bear-frog-chess/static/js/runtime~main.37fbcd2b.js"
+  }
+]);
